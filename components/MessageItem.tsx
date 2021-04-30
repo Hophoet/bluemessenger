f9a70abd8acf88ea4b52c7ea3a38b00f762e2836
hophoet <hophoet@gmail.com>
2021-04-30 15:05:37 +0000

    (components) - add messages item conmponent

---
+++ components/MessageItem.tsx
@@ -0,0 +1,82 @@
+
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, FlatList, View, Image, Button, Text, Dimensions, TouchableOpacity} from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
+
+type Props = {
+	message:any
+}
+
+const  MessageItem = (props:Props) =>  {
+
+		useEffect( () => {
+		})
+
+		return (
+			<View style={
+				[styles.container, 
+					(props.message.author!)
+					?styles.sContainer
+					:styles.rContainer]
+			}>
+				<Image style={styles.image} source={require('../assets/user.jpg')}/>
+				<View style={
+					[styles.messageContainer,
+						(props.message.author!)
+						?styles.sMessageContainer
+						:styles.rMessageContainer
+
+				]}>
+					<Text>{props.message.text}</Text>
+				</View>
+			</View>
+
+
+		)
+}
+
+export default MessageItem;
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+	container:{
+		//backgroundColor:'red',
+		maxWidth:width/1.3,
+		alignItems:'flex-end',
+		marginBottom:10
+	},
+	sContainer:{
+		alignSelf:'flex-end',
+		flexDirection:'row-reverse',
+		justifyContent:'center',
+	},
+	rContainer:{
+		alignSelf:'flex-start',
+		flexDirection:'row',
+		justifyContent:'center',
+	},
+	image:{
+		width:width/8,
+		height:width/8,
+		borderRadius:90,
+	},
+	messageContainer:{
+		flex:1,
+		justifyContent:'center',
+		alignItems:'center',
+		paddingVertical:10,
+		paddingHorizontal:5,
+	},
+	sMessageContainer:{
+		backgroundColor:'#39f2',
+		borderBottomLeftRadius:20,
+		borderTopLeftRadius:20,
+		borderTopRightRadius:20
+	},
+	rMessageContainer:{
+		backgroundColor:'#1597bb',
+		borderBottomRightRadius:20,
+		borderTopLeftRadius:20,
+		borderTopRightRadius:20
+	}
+})
