309dc6004821315849c093ad7e3a030d83346cbf
hophoet <hophoet@gmail.com>
2021-04-30 12:16:16 +0000

    (components) - set the conversations navigator component as the default component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LoginScreen from './screens/authentication/Login';
+import Conversations from './screens/main/Conversations';
+import { NavigationContainer } from '@react-navigation/native';
+import Nav from './navigations/Main';
+
 
 
  const App = () => {
    return (
-		<>
-			<LoginScreen/>
-		</>
+		<Nav/>
    );
  };
 
