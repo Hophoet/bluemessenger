2f4e04d955ab0338f56dd611711f385c814a02a0
hophoet <hophoet@gmail.com>
2021-04-30 15:06:59 +0000

    (screens) - add Message listing and the text input for the message sending components

--- screens/main/ConversationDetail.tsx
+++ screens/main/ConversationDetail.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import {Text, View, TextInput, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons";
+import Messages from '../../components/Messages';
 
 type Props = {
 }
@@ -13,8 +14,6 @@ type State = {
 export default class ConversationDetail extends React.Component<Props, State>{
 	constructor(props:Props){
 		super(props);
-		this.state = {
-		}
 	}
 
 
@@ -23,7 +22,24 @@ export default class ConversationDetail extends React.Component<Props, State>{
 		let conversation = this.props.route.params.conversation;
 		return (
 			<View style={styles.container}>
-				<Text>MESSAGES of {conversation.userPhoneNumber}</Text>
+				<View style={styles.messagesContainer}>
+					<Messages messages={conversation.messages}/>
+				</View>
+				<View style={styles.formContainer}>
+					<TouchableOpacity style={styles.fButton}>
+						<Icon name='chevron-down' size={25} color='#1597bb'/>
+					</TouchableOpacity>
+					<View style={styles.textInputContainer}>
+						<TextInput 
+							placeholder='Entrer votre message'
+							style={styles.textInput}
+						/>
+					</View>
+					<TouchableOpacity style={styles.sButton}>
+						<Icon name='arrow-forward' size={25} color='#1597bb'/>
+					</TouchableOpacity>
+				</View>
+				
 			</View>
 		)
 	}
@@ -33,8 +49,39 @@ export default class ConversationDetail extends React.Component<Props, State>{
 const styles = StyleSheet.create({
 	container:{
 		flex:1,
+	},
+	messagesContainer:{
+		flex:1,
+	},
+	formContainer:{
+		flexDirection:'row',
+		//backgroundColor:'red',
+		justifyContent:'center',
+		//alignItems:'center',
+		paddingVertical:10,
+	},
+	textInputContainer:{
+		borderColor:'#1597bb',
+		borderWidth:StyleSheet.hairlineWidth,
+		flex:1,
+		marginHorizontal:5,
+		borderRadius:5,
+	},
+	fButton:{
+		backgroundColor:'white',
+		padding:5,
+		borderRadius:90,
+		alignSelf:'center',
 		justifyContent:'center',
 		alignItems:'center',
+		marginHorizontal:5,
+	},
+	sButton:{
+		padding:5,
+		alignSelf:'center',
+		justifyContent:'center',
+		alignItems:'center',
+		marginHorizontal:5,
 	}
 
 })
