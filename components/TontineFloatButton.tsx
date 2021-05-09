6fc8639b3f75fb5be12d4100d4779e103b045274
hophoet <hophoet@gmail.com>
2021-05-09 15:37:50 +0000

    (components) - add Tontine user picking done button

---
+++ components/TontineFloatButton.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {View, Text, StyleSheet, TouchableWithoutFeedback, Animated} from 'react-native'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Icon from "react-native-vector-icons/Ionicons";
+
+export default class FloatButton extends React.Component {
+
+    press = () => {
+    }
+    render(){
+        
+        return (
+            <View style={[styles.container]}>
+                <TouchableWithoutFeedback onPress={this.press}>
+                    <View style={[styles.button,{backgroundColor:'#1597bb'}]}>
+                        <Icon size={40} name={(this.props.icon)?this.props.icon:'call'} color={'white'}/>
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
+          
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center',
+        position:'absolute',
+        bottom:32,
+        right:32,
+		justifyContent:'flex-end',
+		alignItems:'flex-end',
+    
+       
+       
+    },
+    button:{
+        width:70,
+        height:70,
+        borderRadius:100,
+        justifyContent:'center',
+        alignItems:'center',
+        //position:'absolute',
+        elevation:10,
+        //borderWidth:2,
+        //borderColor:'white',
+      
+        
+      
+    },
+})
