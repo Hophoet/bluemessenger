70907474fe98c2fb557f9f38ed0aedc2e7d31c91
hophoet <hophoet@gmail.com>
2021-05-11 19:32:00 +0000

    (components) - update TontineUserItem because of ParameterTontine component props

--- components/TontineUserItem.tsx
+++ components/TontineUserItem.tsx
@@ -8,21 +8,56 @@ type Props = {
 
 const  StoryItem = (props:Props) =>  {
 		const [picked, pick] = useState(false);
+		const [inOrderList, setInOrderList] = useState(false);
+		const [indexInOrderList, setIndexInOrderList] = useState(0);
 
 		useEffect( () => {
+			setUserInOrderList(props.story);
 			if(!props.pickable){
 				pick(true);
 			}
 		})
 
+		const setUserInOrderList = (user) => {
+			if(props.step == 'parameter' && props.customOrderTypePicked){
+				let usersOrder = [...props.usersOrder];
+				let userExists = usersOrder.find(item => item.id == user.id);
+				if(userExists){
+					//usersOrder = usersOrder.filter(item => item.id != user.id);
+					let index = usersOrder.indexOf(userExists)
+					setInOrderList(true);
+					setIndexInOrderList(index+1);
+				}
+				else{
+					setInOrderList(false);
+				}
+			}
+			else if (props.step == 'parameter' && !props.customOrderTypePicked){
+				setInOrderList(true);
+				let usersOrder = [...props.usersOrder];
+				let userExists = usersOrder.find(item => item.id == user.id);
+				let index = usersOrder.indexOf(userExists)
+				setIndexInOrderList(index+1);
+
+			}
+
+		}
+
 		const _toggleUserSet = () => {
 				props.toggleUserSet(props.story);
+		}
+
+		const _toogleUserInTheOrderList = () => {
+			if(props.step == 'parameter'){
+				props.toogleUserInTheOrderList(props.story);
+			}
 		}
 		
 
 		return (
 			<TouchableOpacity 
 				onPress={() =>{
+					_toogleUserInTheOrderList();
 					if(props.pickable){
 						pick(!picked);
 						_toggleUserSet();
@@ -31,7 +66,9 @@ const  StoryItem = (props:Props) =>  {
 				}
 				style={styles.container}>
 				<View style={styles.column1}>
-					<View style={styles.statusDot}/>
+					{ props.step != 'parameter' &&
+						<View style={styles.statusDot}/>
+					}
 					<Image style={styles.image} source={require('../assets/user.jpg')}/>
 				</View>
 				<View style={styles.column2}>
@@ -39,9 +76,32 @@ const  StoryItem = (props:Props) =>  {
 					<Text >Description</Text>
 				</View>
 				<View style={styles.column3}>
-					<TouchableOpacity style={(picked)?styles.pickButton:{}}>
+					{ props.step != 'parameter' &&
+					<TouchableOpacity 
+						style={[
+							(picked)?styles.pickButton:{}
+						]}
+
+					>
 						<Icon name='checkmark' color='white' size={20}/>
 					</TouchableOpacity>
+					}
+					{ props.step == 'parameter' &&
+					<TouchableOpacity 
+						style={[ 
+							(inOrderList)
+								?styles.parameterStepItemPicked
+								:styles.parameterStepItemNotPicked
+						]}
+
+					>
+						{ 
+							inOrderList &&<Text style={styles.orderNumber}>{indexInOrderList}</Text>
+						}
+					</TouchableOpacity>
+
+
+					}
 				</View>
 
 			</TouchableOpacity>
@@ -55,7 +115,8 @@ export default StoryItem;
 const {width, height} = Dimensions.get('window');
 const styles = StyleSheet.create({
 	container:{
-		flexDirection:'row'	
+		flexDirection:'row',
+		marginLeft:5,
 	},
 	column1:{
 		flexDirection:'row',
@@ -97,6 +158,30 @@ const styles = StyleSheet.create({
 		borderRadius:90,
 		justifyContent:'center',
 		alignItems:'center',
+	},
+	parameterStepItemNotPicked:{
+		backgroundColor:'#1112' ,//'#39f2',
+		padding:5,
+		borderRadius:90,
+		justifyContent:'center',
+		width:width/11,
+		height:width/11,
+		alignItems:'center',
+		
+	},
+	parameterStepItemPicked:{
+        backgroundColor: '#1597bb',
+		padding:5,
+		width:width/11,
+		height:width/11,
+		borderRadius:90,
+		justifyContent:'center',
+		alignItems:'center',
+		opacity:1,
+	},
+	orderNumber:{
+		fontSize:20,
+		color:'white',
 	}
 
 })
