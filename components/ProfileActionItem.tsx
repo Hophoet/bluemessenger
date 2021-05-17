1863f043a9d6bae4c7c95ee59c8ab137b4f9cbaa
hophoet <hophoet@gmail.com>
2021-05-17 00:25:02 +0000

    (components) - add profile actions item component ProfileActionItem component

---
+++ components/ProfileActionItem.tsx
@@ -0,0 +1,83 @@
+
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Image, View, Text, Dimensions, TouchableOpacity} from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons";
+
+type Props = {
+}
+
+const  ProfileActionItem = (props:Props) =>  {
+
+		useEffect( () => {
+		})
+
+		return (
+			<TouchableOpacity 
+				style={styles.container}>
+				<View style={styles.column1}>
+					<Text style={styles.actionName}>{props.item.name}</Text>
+				</View>
+				<View style={styles.column2}>
+					{ props.item.number &&
+					<View style={styles.numberContainer}>
+						<Text style={styles.number}>{props.item.number}</Text>
+					</View>
+					}
+				</View>
+			</TouchableOpacity>
+
+
+		)
+}
+
+export default ProfileActionItem;
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+	container:{
+		flexDirection:'row',
+		//backgroundColor:'red',
+		justifyContent:'space-between',
+		flex:1,
+		height:width/6,
+	},
+	column1:{
+		flexDirection:'row',
+		alignItems:'center',
+		flex:1,
+		//backgroundColor:'green',
+		paddingLeft:20,
+	},
+	column2:{
+		flex:1,
+		justifyContent:'center',
+		paddingHorizontal:10,
+		alignItems:'center',
+	},
+	column3:{
+		flexDirection:'row',
+		justifyContent:'center',
+		alignItems:'center',
+		flex:1,
+	},
+	actionName:{
+		fontSize:20,
+		fontWeight:'bold',
+		color:'white',
+	
+	},
+	numberContainer:{
+		backgroundColor:'white',
+		borderRadius:100,
+		width:width/10,
+		height:width/10,
+		justifyContent:'center',
+		alignItems:'center',
+	},
+	number:{
+        color: '#1597bb',
+		fontWeight:'bold',
+		fontSize:18,
+	}
+
+})
