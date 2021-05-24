7a44c0e754534653b8c7e5126fce0b19686ddea2
hophoet <hophoet@gmail.com>
2021-05-24 21:30:03 +0000

    (screens) - set the main navigation component as the main.

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import LoginScreen from './screens/authentication/Login';
 import Conversations from './screens/main/Conversations';
 import { NavigationContainer } from '@react-navigation/native';
-import Nav from './navigations/Main';
-
+import {MainNav} from './navigations/Main';
+import Main from './screens/main/Main';
 
 
  const App = () => {
    return (
-		<Nav/>
+		<MainNav/>
    );
  };
 
